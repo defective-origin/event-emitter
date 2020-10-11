a5aa8200158e3a2760d13d6d827d9288f73d1632
defective-origin <defective.origin@gmail.com>
2020-10-11 16:30:29 +0300

    refactor: tests

--- src/__tests__/test_event_emitter.py
+++ src/__tests__/test_event_emitter.py
@@ -5,63 +5,57 @@ from ..event_emitter import EventEmitter
 
 
 class TestEventEmitter(TestCase):
+
+    @classmethod
+    def setUp(cls):
+        cls.eventEmitter = EventEmitter()
+
     def test_contains(self):
         with self.subTest('should return True if contains event'):
-            eventEmitter = EventEmitter()
+            self.eventEmitter.on('TEST', lambda: None)
 
-            eventEmitter.on('TEST', lambda: None)
-
-            self.assertTrue(eventEmitter.contains('TEST'))
+            self.assertTrue(self.eventEmitter.contains('TEST'))
 
     def test__parse_names(self):
         with self.subTest('should return array of names'):
-            eventEmitter = EventEmitter()
-
-            result = eventEmitter._EventEmitter__parse_names('  a b c  d  ')
+            result = self.eventEmitter._EventEmitter__parse_names('  a b c  d  ')
 
             self.assertTrue(result == ['a', 'b', 'c', 'd'])
 
     def test_enable(self):
         with self.subTest('should enable events'):
-            eventEmitter = EventEmitter()
+            self.eventEmitter.on('TEST_EVENT_1', lambda: None)
+            self.eventEmitter.on('TEST_EVENT_2', lambda: None)
+            self.eventEmitter.enable('TEST_EVENT_1 TEST_EVENT_2')
 
-            eventEmitter.on('TEST_EVENT_1', lambda: None)
-            eventEmitter.on('TEST_EVENT_2', lambda: None)
-            eventEmitter.enable('TEST_EVENT_1 TEST_EVENT_2')
-
-            self.assertTrue(eventEmitter.is_enabled('TEST_EVENT_1'))
-            self.assertTrue(eventEmitter.is_enabled('TEST_EVENT_2'))
+            self.assertTrue(self.eventEmitter.is_enabled('TEST_EVENT_1'))
+            self.assertTrue(self.eventEmitter.is_enabled('TEST_EVENT_2'))
 
     def test_disable(self):
         with self.subTest('should disable events'):
-            eventEmitter = EventEmitter()
+            self.eventEmitter.on('TEST_EVENT_1', lambda: None)
+            self.eventEmitter.on('TEST_EVENT_2', lambda: None)
+            self.eventEmitter.disable('TEST_EVENT_1 TEST_EVENT_2')
 
-            eventEmitter.on('TEST_EVENT_1', lambda: None)
-            eventEmitter.on('TEST_EVENT_2', lambda: None)
-            eventEmitter.disable('TEST_EVENT_1 TEST_EVENT_2')
-
-            self.assertTrue(eventEmitter.is_disabled('TEST_EVENT_1'))
-            self.assertTrue(eventEmitter.is_disabled('TEST_EVENT_2'))
+            self.assertTrue(self.eventEmitter.is_disabled('TEST_EVENT_1'))
+            self.assertTrue(self.eventEmitter.is_disabled('TEST_EVENT_2'))
 
     def test_empty(self):
         with self.subTest('should enable events'):
-            eventEmitter = EventEmitter()
+            self.eventEmitter.on('TEST_EVENT_1', lambda: None)
+            self.eventEmitter.on('TEST_EVENT_2', lambda: None)
+            self.eventEmitter.empty()
 
-            eventEmitter.on('TEST_EVENT_1', lambda: None)
-            eventEmitter.on('TEST_EVENT_2', lambda: None)
-            eventEmitter.empty()
-
-            self.assertTrue(len(eventEmitter._EventEmitter__events) == 0)
+            self.assertTrue(len(self.eventEmitter._EventEmitter__events) == 0)
 
     def test_emit(self):
         with self.subTest('should call events'):
-            eventEmitter = EventEmitter()
             mock_callback1 = Mock()
             mock_callback2 = Mock()
 
-            eventEmitter.on('TEST_EVENT_1', mock_callback1)
-            eventEmitter.on('TEST_EVENT_2', mock_callback2)
-            asyncio.run(eventEmitter.emit('TEST_EVENT_1 TEST_EVENT_2'))
+            self.eventEmitter.on('TEST_EVENT_1', mock_callback1)
+            self.eventEmitter.on('TEST_EVENT_2', mock_callback2)
+            asyncio.run(self.eventEmitter.emit('TEST_EVENT_1 TEST_EVENT_2'))
 
             self.assertTrue(mock_callback1.called == 1)
             self.assertTrue(mock_callback2.called == 1)
